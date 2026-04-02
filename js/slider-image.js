baba7776a81b98f0bb38dd6bb635681487f58068
jkchoy <jackilyn@hawaii.edu>
2026-04-02 13:14:49 -1000

    sizing, runner, images

--- js/slider-image.js
+++ js/slider-image.js
@@ -35,17 +35,17 @@ document.addEventListener("DOMContentLoaded", function () {
     dragger.style.left = pct + "%";
   });
 
-  dragger.addEventListener("touchstart", () => isDown = true);
-  document.addEventListener("touchend", () => isDown = false);
-  document.addEventListener("touchmove", function (e) {
-    if (!isDown) return;
-    const rect = container.getBoundingClientRect();
-    let x = e.touches[0].clientX - rect.left;
-    x = Math.max(0, Math.min(rect.width, x));
-    const pct = (x / rect.width) * 100;
-    afterLayer.style.clipPath = "inset(0 " + (100 - pct) + "% 0 0)";
-    dragger.style.left = pct + "%";
-  });
+  // dragger.addEventListener("touchstart", () => isDown = true);
+  // document.addEventListener("touchend", () => isDown = false);
+  // document.addEventListener("touchmove", function (e) {
+  //   if (!isDown) return;
+  //   const rect = container.getBoundingClientRect();
+  //   let x = e.touches[0].clientX - rect.left;
+  //   x = Math.max(0, Math.min(rect.width, x));
+  //   const pct = (x / rect.width) * 100;
+  //   afterLayer.style.clipPath = "inset(0 " + (100 - pct) + "% 0 0)";
+  //   dragger.style.left = pct + "%";
+  // });
 });
 
 
@@ -59,7 +59,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const buttons = document.querySelectorAll(".newPic-web");
   const afterLayer = document.querySelector(".after-layer-web");
   const dragger = document.getElementById("dragger-web");
-  const container = document.querySelector(".image-compare");
+  const container = document.querySelector(".image-compare-web");
 
   let current = 0;
 
@@ -89,15 +89,15 @@ document.addEventListener("DOMContentLoaded", function () {
     dragger.style.left = pct + "%";
   });
 
-  dragger.addEventListener("touchstart", () => isDown = true);
-  document.addEventListener("touchend", () => isDown = false);
-  document.addEventListener("touchmove", function (e) {
-    if (!isDown) return;
-    const rect = container.getBoundingClientRect();
-    let x = e.touches[0].clientX - rect.left;
-    x = Math.max(0, Math.min(rect.width, x));
-    const pct = (x / rect.width) * 100;
-    afterLayer.style.clipPath = "inset(0 " + (100 - pct) + "% 0 0)";
-    dragger.style.left = pct + "%";
-  });
+  // dragger.addEventListener("touchstart", () => isDown = true);
+  // document.addEventListener("touchend", () => isDown = false);
+  // document.addEventListener("touchmove", function (e) {
+  //   if (!isDown) return;
+  //   const rect = container.getBoundingClientRect();
+  //   let x = e.touches[0].clientX - rect.left;
+  //   x = Math.max(0, Math.min(rect.width, x));
+  //   const pct = (x / rect.width) * 100;
+  //   afterLayer.style.clipPath = "inset(0 " + (100 - pct) + "% 0 0)";
+  //   dragger.style.left = pct + "%";
+  // });
 });
